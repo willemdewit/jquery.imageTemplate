b52d638b9b40c4427b51c025d54769c5284c3984
willemdewit <info@w3wit.nl>
2012-11-16 14:25:01 +0100

    fixed x2 and y2 position, should not include scale

--- jquery.imageTemplate.js
+++ jquery.imageTemplate.js
@@ -17,104 +17,122 @@
 				onInit : function() {},
 				onSelectionChange : function() {},
 				onSelectionStart : function() {},
-				onSelectionEnd : function() {}
+				onSelectionEnd : function() {},
+				debug: false
 			};
 			var options = $.extend(defaults, options);
 			return this.each(function() {
+				if (options.debug)
+					console.group('imageTemplate');
 				var $this = $(this), data = $this.data('imageTemplate');
-
 				// If the plugin hasn't been initialized yet
 				if (!data) {
-					var overlayWidth, overlayHeight, overlayOutlineOffset;
-					if (options.width > options.height) {
-						overlayWidth = options.width - options.height;
-						overlayHeight = 0;
-						overlayOutlineOffset = options.height / 2;
+					if (options.debug)
+						console.log('plugin not yet initialized, initialize now');
+
+					var loadFunction = function() {
+						if (options.debug)
+							console.log('imageLoaded');
+						var overlayWidth, overlayHeight, overlayOutlineOffset;
+						if (options.width > options.height) {
+							overlayWidth = options.width - options.height;
+							overlayHeight = 0;
+							overlayOutlineOffset = options.height / 2;
+						} else {
+							overlayWidth = 0;
+							overlayHeight = options.height - options.width;
+							overlayOutlineOffset = options.width / 2;
+						}
+	
+						var $imageTemplateWrapper = $('<div></div>')
+								.addClass('imageTemplate_wrapper'), 
+							$imageTemplateOverlay = $('<div></div>')
+								.addClass('imageTemplate_overlay')
+								.css({
+									width : overlayWidth,
+									height : overlayHeight,
+									top : '50%',
+									left : '50%',
+									marginLeft : (overlayWidth / 2) * -1,
+									marginTop : (overlayHeight / 2) * -1,
+									outlineOffset : overlayOutlineOffset + 'px'
+								})
+								.appendTo($imageTemplateWrapper),
+							originalImageWidth = $this.width(), 
+							originalImageHeight = $this.height();
+						var imageTemplateWrapperWidth = (2 * originalImageWidth) - options.width, 
+							imageTemplateWrapperHeight = (2 * originalImageHeight) - options.height, 
+								draggableOptions = {
+								drag : function(e, ui) {
+									options.onSelectionChange($imageTemplateSubject,$this.imageTemplate('getSelection'));
+								},
+								start : function(e, ui) {
+									options.onSelectionStart($imageTemplateSubject,$this.imageTemplate('getSelection'));
+								},
+								stop : function(e, ui) {
+									options.onSelectionEnd($imageTemplateSubject,$this.imageTemplate('getSelection'));
+								}
+							}, 
+							resizableOptions = {
+								handles: 'ne,se,sw,nw',
+								resize : function(e, ui) {
+									options.onSelectionChange($imageTemplateSubject,$this.imageTemplate('getSelection'));
+								},
+								start : function(e, ui) {
+									options.onSelectionStart($imageTemplateSubject,$this.imageTemplate('getSelection'));
+								},
+								stop : function(e, ui) {
+									options.onSelectionEnd($imageTemplateSubject,$this.imageTemplate('getSelection'));
+								}
+							}, 
+							$image = $('<div></div>')
+								.css({
+									background : 'url(' + $this.attr('src')	+ ') 50% no-repeat',
+									width : originalImageWidth,
+									height : originalImageHeight,
+									backgroundSize : '100% 100%'
+								});
+	
+						if (options.keepRatio) 
+							resizableOptions.aspectRatio = originalImageWidth / originalImageHeight;
+	
+						$imageTemplateWrapper.insertBefore($this);
+						$this.replaceWith($image);
+						$imageTemplateSubject = $image
+							.appendTo($imageTemplateWrapper)
+							.addClass('imageTemplate_subject')
+							.draggable(draggableOptions)
+							.resizable(resizableOptions);
+						$imageTemplateWrapper.css({
+							width : imageTemplateWrapperWidth,
+							height : imageTemplateWrapperHeight
+						});
+						$imageTemplateSubject.css({
+							top : (imageTemplateWrapperHeight / 2) - (originalImageHeight / 2),
+							left : (imageTemplateWrapperWidth / 2) - (originalImageWidth / 2)
+						});
+						$this.data('imageTemplate', {
+							target : $this,
+							imageTemplateWrapper : $imageTemplateWrapper,
+							imageTemplateSubject : $imageTemplateSubject,
+							imageTemplateOverlay : $imageTemplateOverlay,
+							options : options,
+							originalImageWidth : originalImageWidth,
+							originalImageHeight : originalImageHeight
+						});
+						options.onInit($imageTemplateSubject, $this.imageTemplate('getSelection'));
+					};
+					if ($this.width() > 0) {
+						loadFunction.call(this);
 					} else {
-						overlayWidth = 0;
-						overlayHeight = options.height - options.width;
-						overlayOutlineOffset = options.width / 2;
+						$this.get(0).onload = loadFunction;
 					}
-
-					var $imageTemplateWrapper = $('<div></div>')
-							.addClass('imageTemplate_wrapper'), 
-						$imageTemplateOverlay = $('<div></div>')
-							.addClass('imageTemplate_overlay')
-							.css({
-								width : overlayWidth,
-								height : overlayHeight,
-								top : '50%',
-								left : '50%',
-								marginLeft : (overlayWidth / 2) * -1,
-								marginTop : (overlayHeight / 2) * -1,
-								outlineOffset : overlayOutlineOffset + 'px'
-							})
-							.appendTo($imageTemplateWrapper),
-						originalImageWidth = $this.width(), 
-						originalImageHeight = $this.height(), 
-						imageTemplateWrapperWidth = (2 * originalImageWidth) - options.width, 
-						imageTemplateWrapperHeight = (2 * originalImageHeight) - options.height, 
-							draggableOptions = {
-							drag : function(e, ui) {
-								options.onSelectionChange($imageTemplateSubject,$this.imageTemplate('getSelection'));
-							},
-							start : function(e, ui) {
-								options.onSelectionStart($imageTemplateSubject,$this.imageTemplate('getSelection'));
-							},
-							stop : function(e, ui) {
-								options.onSelectionEnd($imageTemplateSubject,$this.imageTemplate('getSelection'));
-							}
-						}, 
-						resizableOptions = {
-							handles: 'ne,se,sw,nw',
-							resize : function(e, ui) {
-								options.onSelectionChange($imageTemplateSubject,$this.imageTemplate('getSelection'));
-							},
-							start : function(e, ui) {
-								options.onSelectionStart($imageTemplateSubject,$this.imageTemplate('getSelection'));
-							},
-							stop : function(e, ui) {
-								options.onSelectionEnd($imageTemplateSubject,$this.imageTemplate('getSelection'));
-							}
-						}, 
-						$image = $('<div></div>')
-							.css({
-								background : 'url(' + $this.attr('src')	+ ') 50% no-repeat',
-								width : originalImageWidth,
-								height : originalImageHeight,
-								backgroundSize : '100% 100%'
-							});
-
-					if (options.keepRatio) 
-						resizableOptions.aspectRatio = originalImageWidth / originalImageHeight;
-
-					$imageTemplateWrapper.insertBefore($this);
-					$this.replaceWith($image);
-					$imageTemplateSubject = $image
-						.appendTo($imageTemplateWrapper)
-						.addClass('imageTemplate_subject')
-						.draggable(draggableOptions)
-						.resizable(resizableOptions);
-					$imageTemplateWrapper.css({
-						width : imageTemplateWrapperWidth,
-						height : imageTemplateWrapperHeight
-					});
-					$imageTemplateSubject.css({
-						top : (imageTemplateWrapperHeight / 2) - (originalImageHeight / 2),
-						left : (imageTemplateWrapperWidth / 2) - (originalImageWidth / 2)
-					});
-					$(this).data('imageTemplate', {
-						target : $this,
-						imageTemplateWrapper : $imageTemplateWrapper,
-						imageTemplateSubject : $imageTemplateSubject,
-						imageTemplateOverlay : $imageTemplateOverlay,
-						options : options,
-						originalImageWidth : originalImageWidth,
-						originalImageHeight : originalImageHeight
-					});
-					options.onInit($imageTemplateSubject, $this.imageTemplate('getSelection'));
 				} else {
 					return $this;
+				}
+				if (options.debug) {
+					console.log('imageTemplate initialized');
+					console.groupEnd('imageTemplate');
 				}
 			});
 		},
@@ -135,8 +153,8 @@
 				overlayPosition = {
 					left : ($imageTemplateWrapper.width() / 2) - (options.width / 2),
 					top : ($imageTemplateWrapper.height() / 2) - (options.height / 2)
-				}, 
-				subjectPosition = $imageTemplateSubject.position(), 
+				};
+			var subjectPosition = $imageTemplateSubject.position(), 
 				selectionObj = {
 					width : options.width,
 					height : options.height
@@ -145,8 +163,10 @@
 				scaleY = data.originalImageHeight / $imageTemplateSubject.height();
 			selectionObj.x1 = overlayPosition.left - subjectPosition.left;
 			selectionObj.y1 = overlayPosition.top - subjectPosition.top;
-			selectionObj.x2 = Math.round(selectionObj.x1 + (options.width * scaleX));
-			selectionObj.y2 = Math.round(selectionObj.y1 + (options.height * scaleY));
+			//selectionObj.x2 = Math.round(selectionObj.x1 + (options.width * scaleX));
+			//selectionObj.y2 = Math.round(selectionObj.y1 + (options.height * scaleY));
+			selectionObj.x2 = Math.round(selectionObj.x1 + (options.width));
+			selectionObj.y2 = Math.round(selectionObj.y1 + (options.height));
 			return selectionObj;
 		},
 		setSelection: function(selection) {
